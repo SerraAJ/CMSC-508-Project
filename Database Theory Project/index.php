0e2b08ed7b409b77c09aa5655b8d83eb72210d1c
Adrienne Serra <serraaj2@vcu.edu>
2020-04-18 13:50:29 -0400

    Testing SQL connection

--- Database Theory Project/index.php
+++ Database Theory Project/index.php
@@ -1,12 +1,24 @@
+<?php
+require_once "config.php";
+echo "Hello World!";
+
+$sql = "CREATE TABLE accounts(
+    account_username NOT NULL PRIMARY KEY,
+    account_password NOT NULL
+)";
+if(mysqli_query($conn, $sql)){
+    echo "Table created successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+?>
 <!DOCTYPE html>
 <html>
 <body>
 
 <h1>My first PHP page</h1>
 
-<?php
-echo "Hello World!";
-?>
+
 
 </body>
 </html>
