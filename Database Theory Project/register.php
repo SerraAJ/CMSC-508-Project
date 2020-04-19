2b818591c9edd757b9ef6b2f129ceae06ec13204
Adrienne Serra <serraaj2@vcu.edu>
2020-04-19 16:43:18 -0400

    Fixed username already taken error

--- Database Theory Project/register.php
+++ Database Theory Project/register.php
@@ -29,15 +29,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         
         if($stmt = mysqli_prepare($conn, $sql))
         {
-            echo "stmt prepare initialized";
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             $param_username = $username;
             
             if(mysqli_stmt_execute($stmt) == 1)
             {
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1)
+                {
                 $username_error = "This username is already taken.";
-            }
             
+                }
+            }
             mysqli_stmt_close($stmt);
             
         }
