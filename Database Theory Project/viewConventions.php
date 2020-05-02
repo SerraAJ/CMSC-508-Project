873287864c67957e807e5a1a94b42ae45d08ece8
Adrienne Serra <serraaj2@vcu.edu>
2020-05-01 22:43:52 -0400

    Math error on 32

--- Database Theory Project/viewConventions.php
+++ Database Theory Project/viewConventions.php
@@ -29,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         else
         {
             $inspect_con_number = substr( $inspect_con, (strrpos($inspect_con, " ")));
-            $inspect_con_name = substr($inspect_con, 0, strlen($inspect_con) - strrpos($inspect_con, " ") );
+            $inspect_con_name = substr($inspect_con, 0, strrpos($inspect_con, " ") );
             echo $inspect_con_name." ".$inspect_con_number;
             
             $sql = "SELECT convention_name FROM conventions WHERE convention_name = ? AND convention_number = ?";
